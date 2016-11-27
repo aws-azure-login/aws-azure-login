ebeb83b23c2d465b321485bf438bd3c35e20a717
Dave Johnson <dave@dtjohnson.net>
2016-11-26 19:58:59 -0500

    Initial commit.

---
+++ bin/index.js
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+
+"use strict";
+
+const commander = require("commander");
+const Promise = require("bluebird");
+
+const configureProfile = require("../lib/configureProfile");
+const CLIError = require("../lib/CLIError");
+const login = require("../lib/login");
+
+commander
+    .option("--profile <name>", "The name of the profile to log in with (or configure)")
+    .option("--configure", "Configure the profile")
+    .parse(process.argv);
+
+const profileName = commander.profile || "default";
+
+Promise.resolve()
+    .then(() => {
+        if (commander.configure) return configureProfile(profileName);
+        return login(profileName);
+    })
+    .catch(CLIError, err => {
+        console.error(err.message);
+    });
