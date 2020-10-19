aws-azure-login/aws-azure-login

commit cf5c0c7312f31c20fafcf3952ff81884e1d2c57c

fix implicit casting error from f851298

src/login.ts
@@ -228,7 +228,7 @@ const states = [
       await page.keyboard.type(password);
 
       // federation with on-prem ADFS causes an HTTP basic auth prompt inside a redirect here
-      await page.authenticate({'username': username, 'password': password});
+      await page.authenticate({'username': String(username), 'password': String(password)});
 
       debug("Submitting form");
       await page.click("span[class=submit],input[type=submit]");

commit 9ab405bb83137c0e00a97aaff4876ac18a4d5aa5

fix linting errors introduced in cf5c0c7

src/login.ts
@@ -228,7 +228,10 @@ const states = [
       await page.keyboard.type(password);
 
       // federation with on-prem ADFS causes an HTTP basic auth prompt inside a redirect here
-      await page.authenticate({'username': String(username), 'password': String(password)});
+      await page.authenticate({
+        username: String(username),
+        password: String(password),
+      });
 
       debug("Submitting form");
       await page.click("span[class=submit],input[type=submit]");