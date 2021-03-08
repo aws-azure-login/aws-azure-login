0172b71365a41615e91d7b0620cb4e46bdce0472
Oscar Sullivan <oscar.sullivan@jagex.com>
2021-03-08 12:32:43 +0000

    Update unrecognised `error-code` error message (formatting & ADFS section)

--- src/login.ts
+++ src/login.ts
@@ -438,7 +438,11 @@ const states = [
         case "Unable to find `error-code` on page.":
           await page.screenshot({ path });
           throw new CLIError(
-            `Unable to recognize chrome exception! A screenshot has been dumped to ${path}. If this problem persists, try running with --mode=gui or --mode=debug`
+            `Unable to recognize Puppeteer's page state!
+  * A screenshot has been dumped to '${path}'
+  * If the screenshot is blank, try running with '--mode=debug'
+  * If this problem persists, try running with '--mode=gui'
+  * If your organisation uses ADFS, please read the 'ADFS' section in the README`
           );
         default:
           descriptiveError = "Unable to find the error on the page.";
