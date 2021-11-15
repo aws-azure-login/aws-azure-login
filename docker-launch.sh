4284d29e833d80248502abc170a82658da7e0806
Jordi Prats <jordi.prats@gmail.com>
2021-11-15 19:59:39 +0100

    Added env.var and /tmp/X11 volume to use the gui mode using docker

--- docker-launch.sh
+++ docker-launch.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker run --rm -it -v ~/.aws:/root/.aws sportradar/aws-azure-login "$@"
+docker run --rm -it -v ~/.aws:/root/.aws -e DISPLAY="${DISPLAY}" -v /tmp/.X11-unix:/tmp/.X11-unix sportradar/aws-azure-login "$@"
