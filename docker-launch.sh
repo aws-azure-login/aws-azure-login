30f023a3b3d1a9ee47bfc38c6d37a53763c3705e
James Westbrook <james.westbrook@coalfire.com>
2023-05-22 14:23:49 -0700

    Made changes to use local files for Docker image

--- docker-launch.sh
+++ docker-launch.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
-docker run --rm -it -v ~/.aws:/root/.aws sportradar/aws-azure-login "$@"
+# Initiate local build of image, goes very quickly if already built
+docker build . -t aws-azure-login
+
+# Run the container using local image
+docker run --rm -it -v ~/.aws:/root/.aws aws-azure-login "$@"
